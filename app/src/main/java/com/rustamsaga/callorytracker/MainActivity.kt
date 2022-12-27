db028788598491eff1da79285194714d64c07620
RustamSaga <rustam.saga94@gmail.com>
2022-12-27 14:31:46 +0300

    add modules buildSrc, tracker, onboarding, core

---
+++ app/src/main/java/com/rustamsaga/callorytracker/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.rustamsaga.callorytracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.rustamsaga.callorytracker.ui.theme.CaloryTrackerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CaloryTrackerTheme {
+
+            }
+        }
+    }
+}
