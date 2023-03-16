071b4557602ba5b83c05f4aec9e980cb412e2576
RustamSaga <rustam.saga94@gmail.com>
2023-03-16 17:07:46 +0300

    Day Selector

--- tracker/tracker_presentation/src/main/java/com/rustamsaga/tracker_presentation/TrackerOverviewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/rustamsaga/tracker_presentation/TrackerOverviewScreen.kt
@@ -1,8 +1,6 @@
 package com.rustamsaga.tracker_presentation
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -10,7 +8,9 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.rustamsaga.core.util.UiEvent
 import com.rustamsaga.core_ui.LocalSpacing
+import com.rustamsaga.tracker_presentation.components.DaySelector
 import com.rustamsaga.tracker_presentation.components.NutrientsHeader
+import com.rustamsaga.tracker_presentation.tracker_overview.TrackerOverviewEvent
 import com.rustamsaga.tracker_presentation.tracker_overview.TrackerOverviewViewModel
 
 @Composable
@@ -28,6 +28,19 @@ fun TrackerOverviewScreen(
     ){
         item {
             NutrientsHeader(state = state)
+            DaySelector(
+                date = state.date,
+                onPreviousDayClick = {
+                    viewModel.onEvent(TrackerOverviewEvent.OnPreviousDayClick)
+                },
+                onNextDayClick = {
+                    viewModel.onEvent(TrackerOverviewEvent.OnNextDayClick)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = spacing.spaceMedium)
+            )
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
         }
     }
     
