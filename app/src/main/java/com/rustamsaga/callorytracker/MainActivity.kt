2de11f63217bbcca4e9c19d663051272ac982e4e
RustamSaga <rustam.saga94@gmail.com>
2023-03-17 18:00:37 +0300

    Search Text Field UI

--- app/src/main/java/com/rustamsaga/callorytracker/MainActivity.kt
+++ app/src/main/java/com/rustamsaga/callorytracker/MainActivity.kt
@@ -6,10 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import coil.annotation.ExperimentalCoilApi
 import com.rustamsaga.callorytracker.navigation.navigate
 import com.rustamsaga.callorytracker.ui.theme.CaloryTrackerTheme
 import com.rustamsaga.core.navigation.Route
@@ -21,11 +26,15 @@ import com.rustamsaga.onboarding_presentation.height.HeightScreen
 import com.rustamsaga.onboarding_presentation.nutrient_goal.NutrientGoalScreen
 import com.rustamsaga.onboarding_presentation.weight.WeightScreen
 import com.rustamsaga.onboarding_presentation.welcome.WelcomeScreen
+import com.rustamsaga.tracker_presentation.search.SearchScreen
 import com.rustamsaga.tracker_presentation.tracker_overview.TrackerOverviewScreen
 import dagger.hilt.android.AndroidEntryPoint
 
+@ExperimentalComposeUiApi
+@ExperimentalCoilApi
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -85,8 +94,36 @@ class MainActivity : ComponentActivity() {
                                 onNavigate = navController::navigate
                             )
                         }
-                        composable(Route.SEARCH){
+                        composable(
+                            route = Route.SEARCH + "/{mealName}/{dayOfMonth}/{month}/{year}",
+                            arguments = listOf(
+                                navArgument("mealName") {
+                                    type = NavType.StringType
+                                },
+                                navArgument("dayOfMonth") {
+                                    type = NavType.IntType
+                                },
+                                navArgument("month") {
+                                    type = NavType.IntType
+                                },
+                                navArgument("year") {
+                                    type = NavType.IntType
+                                },
+                            )){
+                            val mealName = it.arguments?.getString("mealName")!!
+                            val dayOfMonth = it.arguments?.getInt("dayOfMonth")!!
+                            val month = it.arguments?.getInt("month")!!
+                            val year = it.arguments?.getInt("year")!!
 
+                            SearchScreen(
+                                scaffoldState = scaffoldState,
+                                mealName = mealName,
+                                dayOfMonth = dayOfMonth,
+                                month = month,
+                                year = year,
+                                onNavigateUp = {
+                                    navController.navigateUp()
+                                })
                         }
 
                     }
--- tracker/tracker_presentation/src/main/java/com/rustamsaga/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/rustamsaga/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -15,6 +16,7 @@ import com.rustamsaga.tracker_presentation.R
 import com.rustamsaga.tracker_presentation.tracker_overview.TrackerOverviewEvent
 import com.rustamsaga.tracker_presentation.tracker_overview.TrackerOverviewViewModel
 import com.rustamsaga.tracker_presentation.tracker_overview.components.*
+import kotlinx.coroutines.flow.collect
 
 @ExperimentalCoilApi
 @Composable
@@ -25,6 +27,14 @@ fun TrackerOverviewScreen(
     val spacing = LocalSpacing.current
     val state = viewModel.state
     val context = LocalContext.current
+    LaunchedEffect(key1 = context){
+        viewModel.uiEvent.collect { event ->
+            when(event){
+                is UiEvent.Navigate -> onNavigate(event)
+                else -> Unit
+            }
+        }
+    }
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
