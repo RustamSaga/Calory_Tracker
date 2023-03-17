b542da325b258ffed0626d5dea73a3c1f68594e9
RustamSaga <rustam.saga94@gmail.com>
2023-03-17 18:44:45 +0300

    Bug Fixes

--- app/src/main/java/com/rustamsaga/callorytracker/MainActivity.kt
+++ app/src/main/java/com/rustamsaga/callorytracker/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.navigation.navArgument
 import coil.annotation.ExperimentalCoilApi
 import com.rustamsaga.callorytracker.navigation.navigate
 import com.rustamsaga.callorytracker.ui.theme.CaloryTrackerTheme
+import com.rustamsaga.core.domain.preferences.Preferences
 import com.rustamsaga.core.navigation.Route
 import com.rustamsaga.onboarding_presentation.activity.ActivityScreen
 import com.rustamsaga.onboarding_presentation.age.AgeScreen
@@ -29,14 +30,19 @@ import com.rustamsaga.onboarding_presentation.welcome.WelcomeScreen
 import com.rustamsaga.tracker_presentation.search.SearchScreen
 import com.rustamsaga.tracker_presentation.tracker_overview.TrackerOverviewScreen
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @ExperimentalComposeUiApi
 @ExperimentalCoilApi
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    @Inject
+    lateinit var preferences: Preferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val shouldShowOnboarding = preferences.loadShouldShowOnboarding()
         setContent {
             CaloryTrackerTheme {
                 val navController = rememberNavController()
@@ -47,7 +53,11 @@ class MainActivity : ComponentActivity() {
                 ) {
                     NavHost(
                         navController = navController,
-                        startDestination = Route.WELCOME
+                        startDestination = if(shouldShowOnboarding) {
+                            Route.WELCOME
+                        }else {
+                            Route.TRACKER_OVERVIEW
+                        }
                     ){
                         composable(Route.WELCOME){
                             WelcomeScreen(onNavigate = navController::navigate)
