560dbc7b32f8a2f24dd7dbe2ac53e8d75ee16371
RustamSaga <rustam.saga94@gmail.com>
2023-03-17 11:26:51 +0300

    SearchViewModel

---
+++ tracker/tracker_presentation/src/main/java/com/rustamsaga/tracker_presentation/search/TrackableFoodUiState.kt
@@ -0,0 +1,9 @@
+package com.rustamsaga.tracker_presentation.search
+
+import com.rustamsaga.tracker_domain.model.TrackableFood
+
+data class TrackableFoodUiState(
+    val food: TrackableFood,
+    val isExpanded: Boolean = false,
+    val amount: String = ""
+)
